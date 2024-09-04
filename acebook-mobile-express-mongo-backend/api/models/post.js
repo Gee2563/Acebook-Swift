832e2204fe916e0412535e198d425d2d589e0994
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-09-04 18:51:03 +0100

    Chore : Remove unneccesary comments, initial user created on db

--- acebook-mobile-express-mongo-backend/api/models/post.js
+++ acebook-mobile-express-mongo-backend/api/models/post.js
@@ -13,9 +13,9 @@ const Post = mongoose.model("Post", PostSchema);
 
 const dateTimeString = new Date().toLocaleString("en-GB");
 new Post({
-  content: `This is a sample post content. createdAt: created at ${dateTimeString}`,
+  content: `This is a sample post after viewPost updates, I should be able to edit. createdAt: created at ${dateTimeString}`,
   createdAt: dateTimeString,
-  userId: "66d601a5bd717d3e9960b8a0",
+  userId: "66d855c0499b799dc0716c20",
   imgUrl: "assets/sample-post-image.png",
   likes: ["66d731ccce2cb3c296a99979", "66d731c90f4e2eac9525194d"],
 }).save();
